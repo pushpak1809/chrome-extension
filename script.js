e532bbf425a4755bf22242643617fe53099ad3c1
pushpak1809 <prajjvalhiwase@gmail.com>
2021-10-02 18:43:10 +0530

    chrome extension

---
+++ script.js
@@ -0,0 +1,7 @@
+fetch("https://icanhazdadjoke.com/slack")
+  .then((data) => data.json())
+  .then((jokeData) => {
+    const jokeText = jokeData.attachments[0].text;
+    const joke = document.getElementById("joke");
+    joke.innerHTML = jokeText;
+  });
